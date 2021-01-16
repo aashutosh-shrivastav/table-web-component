1541eaa4700a0d8c862f025167b854f2a5b56083
aashutosh-shrivastav <ashuism7@gmail.com>
2021-01-16 07:24:12 +0000

    simple rendering

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,23 +1,32 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule, Injector} from '@angular/core';
 
-import { AppRoutingModule } from './app-routing.module';
-import { AppComponent } from './app.component';
+//import { AppRoutingModule } from './app-routing.module';
+//import { AppComponent } from './app.component';
 import { TableCompComponent } from './components/table-comp/table-comp.component';
 import  { createCustomElement } from '@angular/elements';
 
 @NgModule({
   declarations: [
-    AppComponent,
+    //AppComponent,
     TableCompComponent
   ],
   imports: [
-    BrowserModule,
-    AppRoutingModule
+    BrowserModule
+
   ],
   providers: [],
-  bootstrap: [     AppComponent
-]
+  entryComponents: [TableCompComponent]
 })
 export class AppModule {
+    constructor(
+    private injector: Injector
+  ){
+
+  }
+
+  ngDoBootstrap(){
+    const component = createCustomElement(TableCompComponent, {injector: this.injector});
+    customElements.define('app-table-comp', component);
+  }
  }
